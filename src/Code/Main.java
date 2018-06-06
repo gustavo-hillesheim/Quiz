6429d6fadf7af65f75a5b929c70bf330e8b50756
104977 <104977@SALA215.proway.treina>
2018-06-06 20:18:39 -0300

    Arrumada a geração de panel 06/06

--- src/Code/Main.java
+++ src/Code/Main.java
@@ -1,7 +1,11 @@
 package Code;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JScrollPane;
+import javax.swing.JPanel;
 
 public class Main {
 
@@ -10,8 +14,9 @@ public class Main {
 		JFrame frame = new JFrame();
 		frame.setSize(300, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
 		
-		Jogador j1 = new Jogador("Diogo");
+		/*Jogador j1 = new Jogador("Diogo");
 		j1.finalizarJogo(5, 200);
 		
 		Jogador j2 = new Jogador("Nathan");
@@ -21,8 +26,28 @@ public class Main {
 		
 		pane.setSize(200, 200);
 		
+		frame.add(pane);*/
+		
+		Matematica m = new Matematica();
+		
+		JPanel pane = m.getPergunta().getInterface();
+		
+		JButton btn = new JButton("Atualizar");
+		btn.setBounds(200, 200, 100, 50);
+		btn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				m.getPergunta().atualizarPanel();
+			}
+		});
+		
 		frame.add(pane);
+		frame.add(btn);
 		
 		frame.setVisible(true);
+		
+		
 	}
 }
